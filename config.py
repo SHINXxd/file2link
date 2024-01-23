8a92a7dc9db089538d4b072af97cf1095ea6a33b
SHINXxd <151854453+SHINXxd@users.noreply.github.com>
2024-01-24 00:22:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "27570599"))
 API_HASH = os.environ.get("API_HASH", "0329a786739cb73a2b1176582c5e8376")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001829601382"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002042807021"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "5468078784"))
@@ -33,7 +33,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "blade")
 
 #force sub channel id, use "," between each id and add "-100" as prefix
 try:
-    FORCE_SUB_CHANNELS=[-1001811788351, -1002080278271]
+    FORCE_SUB_CHANNELS=[-1001811788351, -1002080278271, -1001848292668]
     for x in (os.environ.get("FORCE_SUB_CHANNELS", "").split()):
         FORCE_SUB_CHANNELS.append(int(x))
 except ValueError:
